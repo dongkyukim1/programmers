d42c7e9fdc9662e7c34e588c187786d67254f9d9
dongkyukim1 <wlsntus55@gmail.com>
2024-06-21 22:26:58 +0900

    집에서

--- src/test.java
+++ src/test.java
@@ -1,5 +1,7 @@
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("하이");
+        System.out.println("집에서");
     }
 }
